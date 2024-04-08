5c0a74efa68b4c86f1658116e042c64b5c75c889
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2024-04-08 03:06:17 -0400

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
-import css from './ContactList.module.css';
+import { useSelector } from "react-redux";
+import css from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
 
-const ContactList = ({ contacts, OnDelite }) => {
-    return (
-      <ul className={css.contList}>
-        {contacts.map(({ id, name, number }) => {
-          return (
-            <li className={css.contactItem} key={id}>
-              <p className="contactText">{name}:</p>
-              <p className="contactText">{number}</p>
-              <button 
-              className={css.btnDelete} 
-              type='button' 
-              onClick={() => OnDelite(id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  };
-  
-  export default ContactList;
+export default function ContactList() {
+  const selectContacts = useSelector(({ contacts: { items } }) => items);
+  const { name } = useSelector(({ filters }) => filters);
 
+  const filteredContacts = selectContacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(name.toLowerCase()) ||
+      contact.number.toLowerCase().includes(name.toLowerCase())
+  );
 
+  return (
+    <ul className={css.contacts_list}>
+      {filteredContacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
+      ))}
+    </ul>
+  );
+}
